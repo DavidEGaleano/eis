33b4c21fd4aae30196a4c476f33d629ac055d848
David Galeano <degnexo@gmail.com>
2015-10-12 20:21:01 -0300

    commit con el test de submarino pasando, que responde que esta destruido si es impactado

--- kata_batallanaval/model/submarino.rb
+++ kata_batallanaval/model/submarino.rb
@@ -1,3 +1,7 @@
 class Submarino < Barco
-
+  
+  def fue_golpeado!(nombre_de_player)
+    super
+    "Submarino del jugador " + @nombre_player + " ha sido destruido!"
+  end
 end
---
+++ kata_batallanaval/spec/sumbarino_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../model/barco'
+require_relative '../model/submarino'
+
+describe 'Submarino' do
+
+  let(:submarino) { Submarino.new("1",1) }  
+
+  it 'cuando es impactado por un proyectil del jugador 2, debe devolver "Submarino del jugador 1 ha sido destruido!"' do
+    expect(submarino.fue_golpeado!("2")).to eq "Submarino del jugador 1 ha sido destruido!"
+  end
+  
+end
