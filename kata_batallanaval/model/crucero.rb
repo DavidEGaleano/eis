9f5c367cd5f8f7bb1a61641d817183631fc30310
David Galeano <degnexo@gmail.com>
2015-10-13 17:11:06 -0300

    commit con el test pasando de crucuero cuando es impactado 2 veces siendo destruido

--- kata_batallanaval/model/crucero.rb
+++ kata_batallanaval/model/crucero.rb
@@ -2,7 +2,11 @@ class Crucero < Barco
 
   def fue_golpeado!(nombre_de_player)
     super
-    "Crucero del jugador " + @player.dar_nombre + " ha sido impactado!"
+    if(!destruido?)
+      "Crucero del jugador " + @player.dar_nombre + " ha sido impactado!"
+    else
+      "Crucero del jugador " + @player.dar_nombre + " ha sido destruido!"
+    end
   end
 
 end
