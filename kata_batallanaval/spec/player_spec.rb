de153f9a332433449f957143e148b6ba8e875d6b
David Galeano <degnexo@gmail.com>
2015-10-13 19:52:47 -0300

    commit elimando el uso de 'should' en algunos test, cambiandolos a expect(obj).to be_kind_of(type)

--- kata_batallanaval/spec/player_spec.rb
+++ kata_batallanaval/spec/player_spec.rb
@@ -10,15 +10,15 @@ describe 'Player' do
   let(:player) { Player.new("1") }  
 
   it 'cuando selecciona un barco con el nombre "crucero" debe devolver un barco tipo "Crucero"' do
-    player.seleccionar_un_barco("crucero").should be_kind_of (Crucero)
+    expect(player.seleccionar_un_barco("crucero")).to be_kind_of (Crucero)
   end
 
   it 'cuando selecciona un barco con el nombre "submarino" debe devolver un barco tipo "Submarino"' do
-    player.seleccionar_un_barco("submarino").should be_kind_of(Submarino)
+    expect(player.seleccionar_un_barco("submarino")).to be_kind_of(Submarino)
   end
 
   it 'cuando selecciona un barco con el nombre "destructor" debe devolver un barco tipo "Destructor"' do
-    player.seleccionar_un_barco("destructor").should be_kind_of (Destructor)
+    expect(player.seleccionar_un_barco("destructor")).to be_kind_of (Destructor)
   end
 
   it 'al solicitarle el nombre al player debe devolver su nombre ' do
