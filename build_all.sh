bd211f96c4732ea741ebf5e45839a00c129799e4
David Galeano <degnexo@gmail.com>
2015-10-17 17:56:17 -0300

    commit eliminando la carpeta kata_piedra-papel-tijeras

--- build_all.sh
+++ build_all.sh
@@ -8,10 +8,10 @@ cd kata_chopper
 bundle install
 bundle exec rake
 cd ..
-cd kata_piedra-papel-tijeras
-bundle install
-bundle exec rake
-cd ..
+#cd kata_piedra-papel-tijera
+#bundle install
+#bundle exec rake
+#cd ..
 cd kata_batallanaval
 bundle install
 bundle exec rake
