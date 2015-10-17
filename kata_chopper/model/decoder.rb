91319893b9e0d680f578d4542e4ab3e88623ff46
David Galeano <degnexo@gmail.com>
2015-10-17 16:54:42 -0300

    commit con el sexto test de chopper 'sum de [1,3] deberia ser cuatro' pasando

--- kata_chopper/model/decoder.rb
+++ kata_chopper/model/decoder.rb
@@ -5,6 +5,9 @@ class Decoder
   def initialize
     @number_to_word_table = Hash.new (String::new)
     @number_to_word_table.store("1", "uno")
+    @number_to_word_table.store("2", "dos")
+    @number_to_word_table.store("3", "tres")
+    @number_to_word_table.store("4", "cuatro")
   end
 
   def get_result_in_words(list)
