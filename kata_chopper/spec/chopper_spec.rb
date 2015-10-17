abde251071e1d0659f6b14e5c4728393cc363ae6
David Galeano <degnexo@gmail.com>
2015-10-17 17:16:00 -0300

    commit con el noveno test de chopper 'sum de [50,49] deberia ser nueve,nueve' pasando

--- kata_chopper/spec/chopper_spec.rb
+++ kata_chopper/spec/chopper_spec.rb
@@ -43,11 +43,11 @@ describe 'Chopper' do
 		expect(chopper.sum([50,50])).to eq 'demasiado grande'
 	end
 
-=begin	
 	it 'sum de [50,49] deberia ser nueve,nueve' do
 		expect(chopper.sum([50,49])).to eq 'nueve,nueve'
 	end
 
+=begin	
 	it 'sum de [0,0] deberia ser cero' do
 		expect(chopper.sum([0,0])).to eq 'cero'
 	end
