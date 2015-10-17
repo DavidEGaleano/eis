99d8d44387d7510858b2a9b468b8de4be07b2734
David Galeano <degnexo@gmail.com>
2015-10-17 15:30:00 -0300

    commit eliminando las carpetas de kata_chopper

--- build_all.sh
+++ build_all.sh
@@ -4,10 +4,10 @@ cd ejemplo_banco
 bundle install
 bundle exec rake
 cd ..
-cd kata_chopper
-bundle install
-bundle exec rake
-cd ..
+#cd kata_chopper
+#bundle install
+#bundle exec rake
+#cd ..
 cd kata_piedra-papel-tijeras
 bundle install
 bundle exec rake
--- kata_chopper_reentrega/model/chopper.rb
+++
@@ -1,9 +0,0 @@
-class Chopper
-  
-  def chop(index,list)
-    if(list.empty?)
-    	-1
-    end
-  end
-
-end
