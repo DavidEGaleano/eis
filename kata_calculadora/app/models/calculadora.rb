f64e144cda6d128299aa14b5909f68cf76493ef8
David Galeano <degnexo@gmail.com>
2015-10-19 15:46:24 -0300

    commit con el test de calculadora 'no hizo operaciones, deberia dar 0' pasando

--- kata_calculadora/app/models/calculadora.rb
+++ kata_calculadora/app/models/calculadora.rb
@@ -1,6 +1,16 @@
 class Calculadora
+  attr_reader :cantidad_operaciones
+  def initialize
+    @cantidad_operaciones = 0
+  end
+
   def sumar(operando_1,operando_2)
+  	@cantidad_operaciones = @cantidad_operaciones+1
     operando_1 + operando_2
+  end
+
+  def cantidad_operaciones
+   @cantidad_operaciones
   end
 
 end
--- kata_calculadora/spec/calculadora_spec.rb
+++ kata_calculadora/spec/calculadora_spec.rb
@@ -9,4 +9,8 @@ describe 'Calculadora' do
     expect(calculadora.sumar(1,2)).to eq 3
   end
 
+  it 'no hizo operaciones, deberia dar 0' do
+    expect(calculadora.cantidad_operaciones).to eq 0
+  end
+
 end
